7633cae515ccd78db502239b83345509615f7409
Chad Daignault <chad.daignault@cloudcheckr.com>
2021-06-14 21:06:46 -0400

    Add Install-Module Azure AD file

---
+++ Install-Module_AzureAD.ps1
@@ -0,0 +1,4 @@
+# The following PowerShell command will install the AzureAD module.
+# https://docs.microsoft.com/en-us/powershell/azure/active-directory/install-adv2?view=azureadps-2.0#installing-the-azure-ad-module
+
+Install-Module AzureAD
