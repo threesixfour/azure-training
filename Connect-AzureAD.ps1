e7441c4997dc44a18f8507e73c9b8d743e520ed8
Chad Daignault <chad.daignault@cloudcheckr.com>
2021-06-14 21:08:16 -0400

    Add Connect-AzureAD to Local Repo
    File didn't changed; just added it to my local repo.

---
+++ Connect-AzureAD.ps1
@@ -0,0 +1,5 @@
+# The following PowerShell command will have a user connect to Azure Active Directory.
+# After running this command, a window will appear for the user to enter their Active Directory Username and Password.
+# The user will also need to approve their sign on if they MFA is enabled.
+
+Connect-AzureAD
